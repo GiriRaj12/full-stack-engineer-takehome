b3d4a84a5e80ebbf7a2045031aa5a1d9905b659f
Giri Raj <girirajsekar50@gmail.com>
2025-05-19 14:59:40 +0530

    Added backend setup and changes in Dockefile

--- backend/src/SocketServer.ts
+++ backend/src/SocketServer.ts
@@ -1,16 +1,59 @@
 import {Server as SocketServer, Socket} from 'socket.io';
 import { Server as HttpServer } from 'http';
 import { Server as HttpsServer } from 'https';
+import { randomUUID } from 'crypto';
+import { SOCKET_EVENTS } from './utils/socket_enums';
+
+
+const editorNotes = {}
+
+interface CLIENT_EVENT_MESSAGE {
+    id: string, 
+    body: any,
+}
 
 const startSocketServer = (httpServer: HttpServer | HttpsServer) => {
   const io: SocketServer = new SocketServer(httpServer, { cors: {} });
 
+  const editorSpace = io.of('/editor')
+
   const onConnection = async (socket: Socket) => {
-    // Socket connection started
     console.log('Connection started with socket id: ' + socket.id);
+
+    editorSpace.emit(SOCKET_EVENTS.EMITI_CLIENT_UPDATE, editorNotes)
   };
 
-  io.on('connection', onConnection);
+  // const onCreateNote = async (data: any) => {
+  //   const newNote = { id: randomUUID(), value: data};
+  //   EditorValue[newNote.id] = newNote.value;
+
+  //   const emitMessage: CLIENT_EVENT_MESSAGE = {
+  //     id: newNote.id,
+  //     body: newNote.value,
+  //   }
+
+  //   editorSpace.emit(SOCKET_EVENTS.EMIT_CLIENT_CREATE, emitMessage)
+  // }
+
+  // const onUpdateNode = async (data: any) => {
+  //   EditorValue[data["id"]] = data["value"]
+
+  //   const emitMessage: CLIENT_EVENT_MESSAGE = {
+  //     id: data["id"],
+  //     body: data["value"],
+  //   }
+
+  //   editorSpace.emit(SOCKET_EVENTS.EMITI_CLIENT_UPDATE, emitMessage)
+  // }
+
+
+  editorSpace.on(SOCKET_EVENTS.CONNECTION, onConnection);
+  // editorSpace.on(SOCKET_EVENTS.CREATE_NOTES, onCreateNote);
+  // editorSpace.on(SOCKET_EVENTS.UPDATE_NOTES, onUpdateNode);
+
+  io.on(SOCKET_EVENTS.DISCONNECT, () => {
+    console.log("Disconnected")
+  })
 };
 
 export default startSocketServer;
---
+++ backend/src/utils/socket_enums.ts
@@ -0,0 +1,11 @@
+
+
+export enum SOCKET_EVENTS {
+    CONNECTION = 'connection',
+    DISCONNECT = 'disconnect',
+    CREATE_NOTES = 'create_note',
+    UPDATE_NOTES = 'update_note',
+
+    EMITI_CLIENT_UPDATE = "client::update",
+    EMIT_CLIENT_CREATE = "client::create"
+}
