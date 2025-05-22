5f9d3b9d5f608a9eb6ded7d48fbfb52aad8d4fb7
Giri Raj <girirajsekar50@gmail.com>
2025-05-22 10:33:56 +0530

    Worked on Frontend pieces 1. Socket Connection and Fixes 2. Editor Notes Page - with responsive updates in Notes Table View Component 3. Updates in way to handle updates from Server Socket

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,92 +1,47 @@
 import React, { useEffect, useState } from "react";
 import Container from "@material-ui/core/Container";
-import Box from "@material-ui/core/Box";
-import { CircularProgress, TextField, Typography } from "@material-ui/core";
-import { io, Socket } from 'socket.io-client';
+import { Typography } from "@material-ui/core";
 
-import EditorComponent from './EditorComponent'
 import UserNameInputComponent from "./UserNameComponent";
+import AvailableNotesComponent from "./AvailableNotesComponent";
 
-
-const SOCKET_URL = "http://localhost:3005/editor"
-
-const USER_SESSION_NAME_KEY = "Draft-Editor-Session-User-Name-Key"
+const USER_SESSION_NAME_KEY = "Draft-Editor-Session-User-Name-Key";
 
 export default function App() {
-  
-  const [connectionMessage, setConnectionMessage] = useState("")
-  const [socketConnection, setSocketConnection] = useState()
-  const [isConnected, setIsConnected] = useState(false)
-
-
-  const [sessionUserName, setSessionUserName] = useState("")
-  const [notes, setNotes] = useState({})
-
+  const [sessionUserName, setSessionUserName] = useState("");
+  const [backendURL, setBakendURL] = useState("http://localhost:3005");
 
   useEffect(() => {
-    console.log("Connecting ! ")
-    const connection = io(SOCKET_URL)
-    setSocketConnection(connection);
+    console.log("localstorage", localStorage.getItem(USER_SESSION_NAME_KEY));
+    setSessionUserName(localStorage.getItem(USER_SESSION_NAME_KEY) || "");
 
+    const backenURL: string | undefined = process.env.BACKEND_URL;
 
-    connection.on("connect", () => {
-      console.log("Connected to server")
-      setIsConnected(true)
-      setConnectionMessage(`Connected as ${connection.id}`)
-    })
+    if (backenURL) setBakendURL(backenURL);
+    else console.log("NO BACKEND URL DECLARED !");
+  }, []);
 
-    
-    connection.on("client::update", (data: any) => {
-
-        console.log("Recieved this", data)
-        setNotes(data);
-    })
-
-
-    connection.on("connect_error", () => {
-      setIsConnected(false)
-      setConnectionMessage("Error occrued while conntecting ! Please try later")
-    })
-
-    console.log("localstorage", localStorage.getItem(USER_SESSION_NAME_KEY))
-    setSessionUserName(localStorage.getItem(USER_SESSION_NAME_KEY) || "")
-
-
-    return () => {
-      connection.disconnect();
-    };
-  }, [])
-
-
-
-
-  const continueSessionAfterUserName = (value) => {
-      console.log("Continue session", value)
-      localStorage.setItem(USER_SESSION_NAME_KEY, value)
-      setSessionUserName(value);
-  }
-
-  const getUserOrEditorComponent = () => {
-    console.log("session user name", sessionUserName)
-
-    if(sessionUserName){
-      console.log("Into session user name component")
-      return <EditorComponent notes={notes} socket={socketConnection} username={sessionUserName}/>
-    }
-
-    return <UserNameInputComponent continueSession={continueSessionAfterUserName} />
-  }
-
+  const continueSessionAfterUserName = value => {
+    console.log("Continue session", value);
+    localStorage.setItem(USER_SESSION_NAME_KEY, value);
+    setSessionUserName(value);
+  };
 
   return (
-    <Container maxWidth="md">
-      <Typography>Joy Collaborative Editor !</Typography>
-      {
-        connectionMessage ? 
-        <Typography>{connectionMessage} {sessionUserName ? `, Editing note as ${sessionUserName}` : ""}</Typography> :
-        <CircularProgress/>
-      }
-      { isConnected ? getUserOrEditorComponent() : "" }
+    <Container maxWidth="md" style={{ marginTop: "5%" }}>
+      <Typography style={{ width: "100%", textAlign: "center", margin: 5 }}>
+        Joy Collaborative Editor !
+      </Typography>
+      {sessionUserName ? (
+        <AvailableNotesComponent
+          userName={sessionUserName}
+          backendURL={backendURL}
+        />
+      ) : (
+        <UserNameInputComponent
+          continueSession={continueSessionAfterUserName}
+        />
+      )}
     </Container>
   );
 }
--- frontend/src/components/EditorComponent.tsx
+++ frontend/src/components/EditorComponent.tsx
@@ -1,20 +1,129 @@
 import React, {useState, useEffect} from 'react'
-import {Editor, EditorState, RichUtils} from 'draft-js';
+import {Editor, EditorState, RichUtils, ContentState} from 'draft-js';
 import 'draft-js/dist/Draft.css';
-import { Button, Container } from '@material-ui/core';
-
+import { Button, CircularProgress, Container, Typography } from '@material-ui/core';
+import { io, Socket } from 'socket.io-client';
 
 function EditorComponent(props){
     
     const [editorState, setEditorState] = useState(EditorState.createEmpty());
+    const [isConnected, setIsConnected] = useState(false);
+    const [connectionMessage, setConnectionMessage] = useState("")
+    const [socket, setSocketConnection] = useState<any>(null);
+    const [isLoadingUpdate, setIsLoadingUpdate] = useState(false);
+
+    const removeConnectionMessage = () => {
+        setTimeout(() => {
+            setConnectionMessage("")
+        }, 5000)
+    }
+
+    useEffect(() => {
+        if(!(props.note && props.note.note_id))
+            return;
+        
+        const SOCKET_URL = `${props.backendURL}/${props.note.note_id}`;
+
+        console.log("Connecting ! ", SOCKET_URL)
+        
+        const socketCon: Socket = io(SOCKET_URL);
+
+        socketCon.on("connect", () => {
+            setSocketConnection(socketCon)
+            console.log("Connected to server")
+
+            setIsConnected(true)
+            setConnectionMessage(`Connected to note with connection id : ${socketCon.id}`)
+            
+            removeConnectionMessage()
+        })
+            
+        socketCon.on("client::update", (data: any) => {
+            console.log("received", data, data.status, data.response)
+            if(!(data && data.status && data.response)){
+                setConnectionMessage("Error while updateing, please refresh")
+                removeConnectionMessage()
+                return;
+            }
+
+
+            console.log("update_event - triggered", data);
+            setIsLoadingUpdate(true)
+            setConnectionMessage("Received an update ! Please wait !")
+            props.setUpdatedNote(data.response)
+            props.pullNotes()
+
+
+            setEditorState(EditorState.createWithContent(ContentState.createFromText(data.response.body)))
+            setIsLoadingUpdate(false)
+            setConnectionMessage("Done updating note !")
+            
+            removeConnectionMessage()
+        })
+
+        socketCon.on("connect_error", () => {
+            setIsConnected(false)
+            setConnectionMessage("Error occrued while conntecting ! Please try later")
+            
+            removeConnectionMessage()
+        })
+
+
+        return () => {
+            socketCon.disconnect();
+        };
+
+    }, [])
+
+
+    useEffect(() => {
+        const currentNote = props.note
+        setEditorState(EditorState.createWithContent(ContentState.createFromText(currentNote.body)))
+    }, [])
+
+
+    const returnBackToTableView = () => {
+        props.returnBackToTableView()
+    }
+
+    const updateNote = () => {
+
+        console.log("updating note !")
+
+        const updateBody = {
+            "note_id": props.note.note_id,
+            "body": editorState.getCurrentContent().getPlainText('\u0001'),
+            "username": props.userName
+        }
+
+        console.log("emitting", updateBody)
+
+        if(!isConnected)
+            return;
+
+        setIsLoadingUpdate(true)
+        socket.emit("update_note", updateBody, (response:any) => {
+            console.log(response);
+            if(response.response){
+                setIsLoadingUpdate(false)
+            } else {
+                setIsLoadingUpdate(false);
+                setConnectionMessage("Cannot update right now, Please try later !")
+                removeConnectionMessage()
+            }
+        })
+    }
     
     return (
-    <Container>
-            <Button onClick={() => setEditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'))}>BOLD</Button>
-            <div style={{border: '1px solid gray', minHeight: '6em'}}>
-                <Editor editorState={editorState} onChange={setEditorState} />;
-            </div>
-    </Container>)
+        <Container>
+                <Button onClick={returnBackToTableView}>BACK</Button>
+                <Typography>Note Name : {props.note['name']}</Typography>
+                <Typography>{connectionMessage}</Typography>
+                <div style={{border: '1px solid gray', minHeight: '6em'}}>
+                    <Editor editorState={editorState} onChange={setEditorState} />;
+                </div>
+                <Button disabled={!isConnected || isLoadingUpdate} onClick={updateNote}>{isLoadingUpdate ? <CircularProgress/> : `Submit`}</Button>
+        </Container>)
 }
 
 export default EditorComponent;
